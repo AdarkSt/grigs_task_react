8578ef10a59f664ed8c6f928d6d29f87159510f6
ft-tiny <tiny@fluxtechs.com>
2023-06-09 21:29:05 +0400

    todo-task-init

---
+++ src/components/ToDoList/index.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux"
+import { removeTodo } from "../../store/features/todosSlice"
+import _map from 'lodash/map'
+
+export const ToDoList = () => {
+
+    const dispatch = useDispatch()
+
+    const todos = useSelector(state => state.todos)
+    const handleRemoveItem = (todoId) => {
+        dispatch(removeTodo({id: todoId}))
+    }
+
+    return (
+        <div>
+            {_map(todos, todo => {
+                return (
+                    <div key={todo.id}>
+                        <div>{todo.title}</div>
+                        <div>{todo.description}</div>
+                        <div>{todo.deadline}</div>
+                        <div>
+                            <button onClick={() => handleRemoveItem(todo.id)}>Remove</button>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
